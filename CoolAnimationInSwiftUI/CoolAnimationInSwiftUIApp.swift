f8c9f7e1d15271c6c6b98682f3abd02a76f0b698
shashwat329 <shashwatrajwanshi41@gmail.com>
2026-04-24 20:45:28 +0530

    Initial Commit

---
+++ CoolAnimationInSwiftUI/CoolAnimationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolAnimationInSwiftUIApp.swift
+//  CoolAnimationInSwiftUI
+//
+//  Created by Kumar Shashwat on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CoolAnimationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
